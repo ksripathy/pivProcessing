d6b3970a44461d6ae26480785bf685811e220f45
ksripathy <ksripathy@icloud.com>
2023-05-07 17:48:34 +0200

    Configured sub-directory locations and python path

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+
+#Configuring relative file locations
+homeDir = os.path.dirname(__file__)
+srcDir = os.path.join(homeDir,"src")
+imDir = os.path.join(homeDir,"images")
+plotDir = os.path.join(homeDir,"plots")
+
