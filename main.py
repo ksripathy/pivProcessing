724ff4c4ee17e4de0fa971702796134f55979471
ksripathy <ksripathy@icloud.com>
2023-05-07 17:50:13 +0200

    Fixed function import issue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 #Configuring relative file locations
 homeDir = os.path.dirname(__file__)
@@ -6,3 +7,8 @@ srcDir = os.path.join(homeDir,"src")
 imDir = os.path.join(homeDir,"images")
 plotDir = os.path.join(homeDir,"plots")
 
+#Add src folder to python path
+sys.path.append(srcDir)
+
+from imageToDataPair import imageToDataPair
+im1Data, im2Data = imageToDataPair(imDir + "/Image_0001_a.tif", imDir + "/Image_0001_b.tif")
